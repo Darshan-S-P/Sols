480cd3dcd85c845f99b82826b3a390a47c256fd3
DarshanSp <119423403+Darshan-S-P@users.noreply.github.com>
2024-07-17 18:49:29 +0530

    token-upload:commit

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -4,7 +4,20 @@ const Branding:FC = ({image,title,message}) => {
   return (
     <div className="ps-4 hidden py-4 lg:block">
       <div className="relative h-full w-full overflow-hidden rounded-xl "> 
-        <img src={`assets/images/ai/${image}.jpg`} alt="" />
+        <img src={`assets/images/ai/${image}.jpg`} alt="" className="h-full w-full -scale-x-100 transform" />
+        <div className="bg-default-950/40 absolute inset-0"> 
+        <div className="flex h-full items-end justify-center">
+          <div className="text-start p-6">
+            <h5 className="mb-3 text-xl font-bold text-white">
+              Solana Token Creator<br/>
+              {title}
+            </h5>
+            <p className="text-default-400 text-base font-medium">
+              {message}
+            </p>
+          </div>
+        </div>
+        </div>
       </div>
     </div>
   );
--- src/views/create/index.tsx
+++ src/views/create/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, useCallback, useState } from 'react';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { Keypair, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
 import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMintInstruction, getMinimumBalanceForRentExemptMint, getAssociatedTokenAddress, createMintToInstruction, createAssociatedTokenAccountInstruction } from '@solana/spl-token';
-import { PROGRAM_ID, createCreateMetadataAccountV3Instruction,createCreateMetadataAccountInstruction } from '@metaplex-foundation/mpl-token-metadata';
+import { PROGRAM_ID, createCreateMetadataAccountV3Instruction } from '@metaplex-foundation/mpl-token-metadata';
 import { notify } from '../../utils/notifications';
 import { ClipLoader } from 'react-spinners';
 import Branding from '../../components/Branding';
@@ -11,9 +11,7 @@ import { useNetworkConfiguration } from 'contexts/NetworkConfigurationProvider';
 //UI part import
 import { AiOutlineClose } from 'react-icons/ai';
 import CreateSVG from '../../components/SVG/CreateSVG';
-import { Upload } from 'lucide';
 import axios from 'axios';
-import { create } from 'domain';
 
 export const CreateView: FC = ({ setOpenCreateModal }) => {
     const { connection } = useConnection();
@@ -78,11 +76,6 @@ export const CreateView: FC = ({ setOpenCreateModal }) => {
                     },
                     isMutable: false,
                     collectionDetails: null,
-
-                  
-                      
-
-                  
                     },
                   }
                 );
@@ -114,10 +107,7 @@ export const CreateView: FC = ({ setOpenCreateModal }) => {
                     tokenATA,
                     publicKey,
                     Number(token.amount)* Math.pow(10, Number(token.decimals))
-
-
                   ),
-
                   createMetadataInstruction
                 );
                 const signature = await sendTransaction(
@@ -125,8 +115,6 @@ export const CreateView: FC = ({ setOpenCreateModal }) => {
                   connection,
                   {
                     signers: [mintKeypair],
-
-
                   }
                 );
                 setTokenMintAddress(mintKeypair.publicKey.toString());
@@ -223,115 +211,148 @@ export const CreateView: FC = ({ setOpenCreateModal }) => {
             <ClipLoader />
           </div>
         )}
-        {tokenMintAddress ? (
-          <section className="flex w-full items-center py-6 px-0 lg:h-screen lg:p-10">
-          <div className="container">
-              <div className="bg-default-950/40 mx-auto max-w-5xl overflow-hidden rounded-2xl backdrop-blur-2xl">
-                  <div className="grid gap-10 lg:grid-cols-2">
-                      <div className="ps-4 hidden py-4 pt-10 lg:block">
-                          <div className="upload relative w-full overflow-hidden rounded-xl">
-                              {token.image ? (
-                                  <img src={token.image} alt="token" className="w-full max-w-full" />
-                              ) : (
-                                  <label htmlFor="file" className="custum-file-upload">
-                                      <div className="icon">
-                                          <CreateSVG />
-                                      </div>
-                                      <div className="text">
-                                          <span>Click To Upload Image</span>
-                                      </div>
-                                      <input type="file" id="file" onChange={handleImageChange} />
-                                  </label>
-                              )}
+        {!tokenMintAddress ? (
+          <section className="flex w-full
+          items-center py-6 px-0 lg:h-screen lg:p-10">
+            <div className="container">
+              <div className="bg-default-950/40
+              mx-auto max-w--5xl overflow-hidden rounded-2xl backdrop-blur-2xl">
+                <div className="grid gap-10 lg:grid-cols-2">
+                  <div className="ps-4 hidden py-4 pt-10 lg:block">
+                    <div className="upload relative w-full overflow-hidden rounded-xl">
+                      {token.image ?(
+                          <img src={token.image} alt="token" className="w-full max-w-full" />
+                        ):(
+                          <label htmlFor="file" className="custum-file-upload">
+                            <div className="icon">
+                              <CreateSVG/>
+                            </div>
+                            <div className="text">
+                              <span>Click To Upload Image</span>
+                            </div>
+                            <input type="file" id="file" onChange={handleImageChange}/>
+                          </label>
+                        )}
+                    </div>
+                    <textarea rows="6" onChange={(e)=>handleFormFieldChange("description", e)} className="border-default-200 relative mt-48 block w-full rounded border-white/10 bg-transparent py-1.5 px-3 text-white/80 focus:border-white/25 focus:ring-transparent"
+                      placeholder="Description of your token ">
+                    </textarea>
+                    </div>
+
+                    <div className="lg:ps-0 flex flex-col p-10">
+                      <div className="pb-6 my-auto">
+                        <h4 className="mb-4 text-2xl font-bold text-white">
+                          Solana Token Creator
+                        </h4>
+                        <p className="text-default-300 mb-8 max-w-sm">
+                          Kindly provide all the details about your token  
+                        </p>
+
+                        <div className="text-start">
+                          <InputView name="Name"
+                          placeholder="name"
+                          clickhandle={(e)=>
+                            handleFormFieldChange
+                            ("name", e)}/>
+                            <InputView name="Symbol"
+                          placeholder="symbol"
+                          clickhandle={(e)=>
+                            handleFormFieldChange
+                            ("symbol", e)}/>
+                            <InputView name="Decimals"
+                          placeholder="decimals"
+                          clickhandle={(e)=>
+                            handleFormFieldChange
+                            ("decimals", e)}/>
+                            <InputView name="Amount"
+                          placeholder="amount"
+                          clickhandle={(e)=>
+                            handleFormFieldChange
+                            ("amount", e)}/>
+                            <div className="mb-6 text-center"> 
+                            <button onClick={()=> createToken(token)} className="bg-primary-600/90 hover:bg-primary-600 group mt-5 inline-flex w-full items-center justify-center rounded-lg px-6 py-2 text-white backdrop-blur-2xl transition-all duration-500
+                            "type="submit">
+                              <span className="fw-bold">Create Token </span>
+                            </button>
                           </div>
-                          <textarea rows="6" onChange={(e) => handleFormFieldChange("description", e)} className="border-default-200 relative mt-48 block w-full rounded border-white/10 bg-transparent py-1.5 px-3 text-white/80 focus:border-white/25 focus:ring-transparent"
-                              placeholder="Description of your token ">
-                          </textarea>
+                        </div>
                       </div>
-                      <div className="lg:ps-0 flex flex-col p-10">
-                          <div className="pb-6 my-auto">
-                              <h4 className="mb-4 text-2xl font-bold text-white">
-                                  Solana Token Creator
-                              </h4>
-                              <div className="flex flex-wrap -mx-2">
-                                  <div className="w-1/2 px-2">
-                                      <input type="text" onChange={(e) => handleFormFieldChange("name", e)} className="border-default-200 block w-full rounded-lg border p-3 text-sm outline-none focus:border-transparent focus:ring-2 focus:ring-amber-600" placeholder="Token Name " />
-                                  </div>
-                                  <div className="w-1/2 px-2">
-                                      <input type="text" onChange={(e) => handleFormFieldChange("symbol", e)} className="border-default-200 block w-full rounded-lg border p-3 text-sm outline-none focus:border-transparent focus:ring-2 focus:ring-amber-600" placeholder="Symbol " />
-                                  </div>
-                              </div>
-                              <div className="flex flex-wrap -mx-2 mt-4">
-                                  <div className="w-1/2 px-2">
-                                      <input type="number" onChange={(e) => handleFormFieldChange("decimals", e)} className="border-default-200 block w-full rounded-lg border p-3 text-sm outline-none focus:border-transparent focus:ring-2 focus:ring-amber-600" placeholder="Decimals " />
-                                  </div>
-                                  <div className="w-1/2 px-2">
-                                      <input type="number" onChange={(e) => handleFormFieldChange("amount", e)} className="border-default-200 block w-full rounded-lg border p-3 text-sm outline-none focus:border-transparent focus:ring-2 focus:ring-amber-600" placeholder="Initial Amount " />
-                                  </div>
-                              </div>
-                              <button className="hover:bg-brand-gradient w-full mt-4 cursor-pointer rounded-lg border bg-brand-700 py-2 px-4 font-medium text-white" onClick={() => createToken(token)}>
-                                  Create Token
-                              </button>
-                          </div>
+
+                      <div>
+                        <div className="text-center"> 
+                          <ul className="flex flex-wrap items-center justify-center gap-2">
+                            <li>
+                              <a onClick={()=>setOpenCreateModal(false)} className="group inline-flex h-10 w-10 items-center justify-center rounded-lg bg-white/20 backdrop-blur-2xl transition-all duration-500 hover:bg-blue-600/60">
+                                <i className="text-2xl text-white group-hover:text-white">
+                                  <AiOutlineClose/>
+                                </i>
+                                </a>
+                            </li>
+                          </ul>
+                        </div>
                       </div>
                   </div>
+                </div>
               </div>
-          </div>
-      </section>
-      
+            </div>  
+          </section>
 
         ):(
-          <section className="flex w-full items-center py-6 px-0 lg:h-screen lg:p-10">
-    <div className="container">
-        <div className="bg-default-950/40 mx-auto max-w-5xl overflow-hidden rounded-2xl backdrop-blur-2xl">
-            {/* //First */}
-            <Branding 
+          <section className="flex w-full
+          items-center py-6 px-0 lg:h-screen lg:p-10">
+            <div className="container">
+              <div className="bg-default-950/40
+              mx-auto max-w--5xl overflow-hidden rounded-2xl backdrop-blur-2xl">
+                <div className="grid gap-10 lg:grid-cols-2">
+                {/* //First */}
+                <Branding 
                 image="auth-img"
                 title="To Build your solana token Creator"
                 message="Try and create your first ever solana project"
-            />
-            {/* SECOND */}
-            <div className="lg:ps-0 flex h-full flex-col p-10">
-                <div className="pb-10">
+                />
+                {/* SECOND */}
+                <div className="lg:ps-0 flex h-full flex-col p-10">
+                  <div className="pb-10">
                     <a className="flex">
-                        <img src="assets/images/logo1.png" alt="" className="h-10" />
+                      <img src="assets/images/logo1.png" alt="" className="h-10" />
                     </a>
-                </div>
-                <div className="my-auto pb-6 text-center"> 
+                  </div>
+                  <div className="my-auto pb-6 text-center"> 
                     <h4 className="mb-4 text-2xl font-bold text-white">
-                        Link to your new token
+                      Link to your new token
                     </h4>
                     <p className="text-default-300 mx-auto mb-5 max-w-sm"> 
-                        Your Solana token is successfully created, Check now explorer
+                      Your Solana token is successfully created,Check now explorer
                     </p>
-                    <div className="flex items-start justify-center overflow-auto max-h-96"> 
-                        <img src={token.image || "assets/images/logo1.png"} alt="Token" className="h-auto max-w-full rounded-xl" />
+                    <div className="flex items-start justify-center"> 
+                      <img src={token.image || "assets/images/logo1.png"} alt="" className="h-40" />
                     </div>
-                    <div className="mt-5 w-full text-center"> 
-                        <p className="text-default-300 text-base font-medium leading-6"> 
-                            <InputView name={"Token Address"} placeholder={tokenMintAddress}/>
-                            <span className="cursor-pointer" onClick={() => navigator.clipboard.writeText(tokenMintAddress)}> 
-                                Copy
-                            </span>
-                        </p>
-                        <div className="mb-6 text-center"> 
-                            <a href={`https://explorer.solana.com/address/${tokenMintAddress}?cluster=${networkConfiguration}`}
-                               target="_blank"
-                               rel="noreferrer"
-                               className="bg-primary-600/90 hover:bg-primary-600 group mt-5 inline-flex w-full items-center justify-center rounded-lg px-6 py-2 text-white backdrop-blur-2xl transition-all duration-500"
-                            >
-                                <span className="fw-bold"> 
-                                    View On Solana
-                                </span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>  
-</section>
 
+                    <div className="mt-5 w-full text-center"> 
+                      <p className="text-default-300 text-base font-medium leading-6"> 
+                        <InputView name={"Token Adress"} placeholder={tokenMintAddress}/>
+                        <span className="cursor-pointer" onClick={()=> navigator.clipboard.writeText(tokenMintAddress)}> 
+                          Copy
+                        </span>
+                      </p>
+                      <div className="mb-6 text-center"> 
+                        <a href={`https://explorer.solana.com/address/${tokenMintAddress}?cluster=${networkConfiguration}`}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="bg-primary-600/90 hover:bg-primary-600 group mt-5 inline-flex w-full items-center justify-center rounded-lg px-6 py-2 text-white backdrop-blur-2xl transition-all duration-500"
+                        >
+                          <span className="fw-bold"> 
+                            View On Solana
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>  
+          </section>
         )}
     </>
     );
